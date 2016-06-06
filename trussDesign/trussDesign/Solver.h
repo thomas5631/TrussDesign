5be676e5637aa0f38e0aaa278b77f1440e9067f8
thomas5631 <thomas.lilley@strath.ac.uk>
2016-06-06 19:38:58 +0100

    The solution to the repeated maxima is in deleting previously maximal rows from the transposed array, then searching for the max in the new (smaller array)
    Not yet implemented

--- trussDesign/trussDesign/Solver.h
+++ trussDesign/trussDesign/Solver.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <algorithm>
+#include <functional>
 
 class Solver
 {
@@ -15,41 +16,49 @@ public:
 	}
 
 
-	static bool abs_compare(int a, int b)
-	{
-		return (abs(a) < abs(b));
-	}
-
-
 	template<typename T>
 	static void pivotise(std::vector<std::vector<T> > &A)
 	{
 		//initializing row and column max pos. vectors
-		std::vector<std::vector<T>> A_T(A[0].size(), std::vector<T>(A.size()));
-		
+		std::vector<unsigned> posHist;
+
 		for (unsigned k = 0; k < A.size(); k++)
 		{
+			std::vector<std::vector<T>> A_T(A[0].size(), std::vector<T>(A.size()));
 			for (unsigned i = 0; i < A.size(); ++i)
 				for (unsigned j = 0; j < A[0].size(); ++j)
 					A_T[j][i] = A[i][j];
+///////////////////////////////////////////The issue lies between these lines////////////////////////////////////////////////////////////
+///////////////////////////////////////////The issue lies between these lines////////////////////////////////////////////////////////////
+			// rows which have previously existed are being deleted, but i am not searching within the new bounds
+			std::sort(posHist.begin(), posHist.end(), std::greater<T>());
+			for (unsigned q = 0; q > posHist.size(); q++)
+			{
+				auto index = posHist[q];
+				A_T.erase(A_T.begin() + q);
+			}
 
 			unsigned pos;
 			if (k == A.size() - 1)
 				pos = A.size() - 1;
 			else
-				pos = std::distance(A_T[k].begin(), std::max_element(A_T[k].begin(), A_T[k].end(), abs_compare));
-			//std::cout << "position: " << pos << std::endl;
+				pos = std::distance(A_T[k].begin(), std::max_element(A_T[k].begin(), A_T[k].end(),
+					[](T a, T b) {return (abs(a) < abs(b)); }));
+			posHist.push_back(pos);
+			std::cout << "Position of max in column " << k << " = " << pos << std::endl;
 			auto r = A[pos][k];
 
 			for (unsigned i = 0; i < A.size(); i++)
 				A[pos][i] = A[pos][i] / r;
+///////////////////////////////////////////The issue lies between these lines////////////////////////////////////////////////////////////
+///////////////////////////////////////////The issue lies between these lines////////////////////////////////////////////////////////////
 
 			//output(A); //issue with 3x3
 
 			for (unsigned j = 0; j < A.size(); j++)
 			{
-				
-				if(j != pos)
+
+				if (j != pos)
 				{
 					auto mfact = A[j][k];
 					for (auto i = 0; i < A[k].size(); ++i)
@@ -58,9 +67,17 @@ public:
 					}
 				}
 			}
+
+
+			std::cout << std::endl;
+			output(A);
 		}
 
-		
+		for (auto p : posHist)
+		{
+			std::cout << p << "\t";
+		}
+
 
 		//auto r = std::max_element(A_T[0].begin(), A_T[0].end());
 		//std::cout<< "Max for this loop: " << r[0] << std::endl;

e87d9092151804635cfeafe4819aab93a3c919be
thomas5631 <thomas.lilley@strath.ac.uk>
2016-06-06 17:39:19 +0100

    Set up precompiled header

--- trussDesign/trussDesign/stdafx.h
+++ trussDesign/trussDesign/stdafx.h
@@ -6,6 +6,17 @@
 #pragma once
 
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include<iomanip>
+
+
+#define GRAVITY -9.81
+#define PI 3.1415926535897
+
+#define IMPERFECTION 0.60    //dimensionless
+#define MODULUS 70E3         //mm^2
+#define SECONDMOMENT 10E4    //mm^4
 
 
 
